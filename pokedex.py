c1548364324930e0c0eaa4f4a229b10c352770f3
Jimmy <43689221+flobbley@users.noreply.github.com>
2019-07-22 23:18:27 -0400

    Add files via upload

--- pokedex.py
+++ pokedex.py
@@ -88,3 +88,49 @@ class pokedex: #fills the global pokedex
         self.pidgey = pokemon('Pidgey', 5, ['normal','flying'], 30, {'attack':70, 'defense':50, 'speed':50}, {1:[wingAttack, 'wing attack'], 2:[gust, 'gust']}, 50, 60, 1.0)
 
 pokedex = pokedex() #actually creates the pokedex
+
+def typeChart(defendType, attackType):
+    base = 1
+    noEffect = 0
+    notVeryEffective = 0.5
+    superEffective = 2
+    types = {'normal':{'fighting':superEffective, 'ghost':noEffect},\
+             'fighting':{'flying':superEffective,'rock':notVeryEffective,'bug':notVeryEffective,'psychic':superEffective},\
+             'flying':{'fighting':notVeryEffective,'ground':noEffect,'rock':superEffective,'bug':notVeryEffective,'grass':notVeryEffective, 'electric':superEffective, 'ice':superEffective},\
+             'poison':{'fighting':notVeryEffective, 'poison':notVeryEffective, 'ground':superEffective,'bug':superEffective,'grass':notVeryEffective, 'psychic':superEffective},\
+             'ground':{'poison':notVeryEffective, 'rock':notVeryEffective, 'water':superEffective, 'grass':superEffective, 'electric':noEffect, 'ice':superEffective},\
+             'rock':{'normal':notVeryEffective, 'fighting':superEffective, 'flying':notVeryEffective, 'poison':notVeryEffective, 'ground':superEffective, 'fire':notVeryEffective, 'water':superEffective, 'grass':superEffective},\
+             'bug':{'fighting':notVeryEffective, 'flying':superEffective, 'poison':superEffective, 'ground':notVeryEffective, 'rock':superEffective, 'fire':superEffective, 'grass':notVeryEffective},\
+             'ghost':{'normal':noEffect, 'fighting':noEffect, 'poison':notVeryEffective, 'bug':notVeryEffective, 'ghost':superEffective},\
+             'fire':{'ground':superEffective, 'rock':superEffective, 'bug':notVeryEffective, 'fire':notVeryEffective, 'water':superEffective, 'grass':notVeryEffective},\
+             'water':{'fire':notVeryEffective, 'water':notVeryEffective, 'grass':superEffective, 'electric':superEffective, 'ice':notVeryEffective},\
+             'grass':{'flying':superEffective, 'poison':superEffective, 'ground':notVeryEffective, 'bug':superEffective, 'fire':superEffective, 'water':notVeryEffective, 'grass':notVeryEffective, 'electric':notVeryEffective, 'ice':superEffective},\
+             'electric':{'flying':notVeryEffective, 'ground':superEffective, 'electric':notVeryEffective},\
+             'psychic':{'fighting':notVeryEffective, 'bug':superEffective, 'ghost':noEffect, 'psychic':notVeryEffective},\
+             'ice':{'fighting':superEffective, 'rock':superEffective, 'fire':superEffective, 'ice':notVeryEffective},\
+             'dragon':{'fire':notVeryEffective, 'water':notVeryEffective, 'grass':notVeryEffective, 'electric':notVeryEffective, 'ice':superEffective, 'dragon':superEffective}}
+    if attackType in types[defendType]:
+        return types[defendType][attackType]
+    else:
+        return base
+    
+def printChart():
+    types = ['normal','fighting','flying','poison','ground','rock','bug','ghost','fire','water','grass','electric','psychic','ice','dragon']
+    print('      ', end = ' ')
+    for poke in types:
+        spacing = 8 - len(poke)
+        if poke == 'dragon':
+            print(poke)
+        else:
+            print(poke+' '*spacing, end = " ")
+    for poke in types:
+        spacing = 8 - len(poke)
+        print(poke+' '*spacing, end = "")
+        for poke1 in types:
+            x = typeChart(poke1, poke)
+            digSpace = 4 - len(str(x))
+            if poke1 == 'dragon':
+                print('   ',x,' '*digSpace)
+            else:
+                print('   ',x,' '*digSpace, end ="")
+        
