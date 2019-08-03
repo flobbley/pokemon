f13ddbea4cc931f1dbc7d732e2d2020839180e6f
Jimmy <43689221+flobbley@users.noreply.github.com>
2019-08-03 17:52:35 -0400

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -948,7 +948,7 @@ def lab(player, pokeGot = True):
                 if menuValid(action, 3):
                     action = int(action)
                     if action == 1:
-                        print('Scientist: "Well hello there young man! Looking for Professor Oak? We haven\'t seen him all morning."')
+                        print('Scientist: "Well hello there',player.name+'!','Looking for Professor Oak? We haven\'t seen him all morning."')
                         input()
                     elif action == 2:
                         return 'palletTown'
